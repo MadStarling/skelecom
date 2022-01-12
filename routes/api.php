c281c0f0a0ed93de601a3ec0c2fa9d3a2b9f2a26
Gabriel Starling <gabriel@scandiweb.com>
2022-01-12 18:57:00 -0300

    SKEL-1
    - Add product model
    - Add product resource
    - Add product migration and seeder

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('products', 'App\Http\Controllers\ProductController@index');
+Route::get('products/{id}','App\Http\Controllers\ProductController@show');
+Route::post('products', 'App\Http\Controllers\ProductController@create');
+Route::put('products/{id}', 'App\Http\Controllers\ProductController@update');
+Route::delete('products/{id}', 'App\Http\Controllers\ProductController@delete');
